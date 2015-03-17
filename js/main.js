17b8283d9026f194c52e9f4725fff075d9bbe0bd
Composite <ukjinplant@msn.com>
2015-03-18 01:16:42 +0900

    Made for nw.js 0.12.0 and clean unused resources.

--- js/main.js
+++ js/main.js
@@ -4,32 +4,48 @@
  */
 ;$(function(){
 
+    // Message Box
 	function msgbox(content, title, buttons){
 		$('#modalContent').html(content);
 		$('#modalTitle').html(title);
 		$modal = $('#commonMotal');
 		buttons = buttons || {};
 
-		var $btns = $modal.find('.modal-footer>button').not('#closeButton').remove().end().end();
+		var $btns = $modal.find('#modalButtons>button').not('#closeButton').remove().end().end();
 		for(var label in buttons){
 			if(label=='close'){
 				$('#closeButton')[!!buttons[label] ? 'show' : 'hide']();
 				continue;
 			}
-			var $btn = $('<button type="button" class="btn btn-default" ></button>')
+			var $btn = $('<button type="button" class="btn btn-gray btn-flat btn-rect btn-small" ></button>')
 			.text(label)
 			.click(buttons[label])
 			.appendTo($btns);
 		}
 
-		$modal.modal({
-			backdrop: 'static',
-			keyboard: false
-		});
-	}
-	msgbox.close = function(){
-		$('#commonMotal').modal('hide');
-	}
+        if(!msgbox.instance){
+            jui.ready([ "ui.modal" ], function(modal) {
+                msgbox.instance = modal("#commonMotal", {
+                    color: "black"
+                });
+                msgbox.close = function(){
+                    msgbox.instance.hide();
+                };
+                $modal.find('#closeButton').on('click', msgbox.close);
+                msgbox.instance.show();
+            });
+        }else{
+            msgbox.instance.show();
+        }
+
+	} msgbox.close = function(){};
+
+    require('nw.gui').Window.get().on('close', function() {
+        if ((commit.insert.length + commit.update.filter(function(a){return a;}).length + commit.remove.length) && confirm('Something changed.\nAre you sure to exit?')) {
+            this.close(true);
+        }
+    });
+
 
 	//공통코드
 	var gender = ['남', '여']
@@ -39,11 +55,13 @@
 	//내장 DB 초기화 및 테이블 생성
 	var db = new Database('demo_insa')
 	,commit = {
-		insert:[],
-		update:[],
-		remove:[]
+        list:[],   //데이터 원본
+		insert:[], //신규 대상
+		update:[], //수정 대상
+		remove:[]  //삭제 대상
 	};
 
+    //DB 초기화
 	db.then(function(){
 		//테이블 정의
 		db.query('CREATE TABLE IF NOT EXISTS insa(id INTEGER PRIMARY KEY ASC, name TEXT, gender TEXT, city TEXT, age INTEGER, part TEXT, rank TEXT, "join" DATETIME)')
@@ -74,8 +92,261 @@
 			});
 	});
 
+    //그리드 처리
+    jui.ready([ "util.base", "ui.dropdown", "uix.table" ], function(_, dropdown, table) {
+        var grid = table("#myGrid", {
+            fields: [ "name", "gender", "city", "age", "part", "rank", "join" ],
+            scroll: true,
+            resize: true,
+            colshow: true,
+            sort: true,
+            editRow: true,
+            event: {
+                //클릭시 행 선택
+                click: function(row, e) {
+                    if($(row.element).hasClass("checked")) {
+                        this.uncheck(row.index);
+                    } else {
+                        this.check(row.index);
+                    }
+                },
+                editstart: function(row) {
+                    row.element.cells[0].firstChild.select();
+                    //편집모드 활성화
+                    $('#btn_insert').data('EDIT_FREEZE', true);
+                },
+                editend: function(row) {
+                    //편집모드 비활성화
+                    $('#btn_insert').data('EDIT_FREEZE', false);
+                }
+            }
+        });
+
+        $(window).on('resize', function(){
+            grid.scroll($('#myGrid').map(function(){return $(this).outerHeight() - $(this).find('thead').outerHeight();}).get(0));
+        }).trigger('resize');
+
+        // 검색 시 필터링 작동
+        $('#search_form').submit(function(e){
+            e.preventDefault();
+            var search = this.elements.search, val = search.value;
+
+            grid.update(val ? commit.list.filter(function(item){return !!~item.name.indexOf(val);}) : commit.list);
+
+            return false;
+        })[0].elements.search.focus();
+
+        //EXCEL DOWNLOAD
+        $('#btn_excel').on('click', function(){
+            $('#excelDownAs').on('change', function(){
+                console.log(this.value);
+                require('fs').writeFileSync(this.value, grid.getCsv());
+                $(this).val('');
+            }).click();
+        });
+
+
+        //신규 버튼
+        $('#btn_insert').click(function(){
+            //편집중에는 제외
+            if ($('#btn_insert').data('EDIT_FREEZE')) {
+                return;
+            }
+
+            var now = new Date(),
+                data = {
+                    "id": now.getTime(),
+                    "name": "새 이름",
+                    "city": "서울",
+                    "gender": "남",
+                    "age": 20,
+                    "part": "총무팀",
+                    "rank": "사원",
+                    "join": [now.getFullYear(), ('00'+(now.getMonth()+1)).slice(-2), ('00'+(now.getDate())).slice(-2)].join('-'),
+                    "isNew": true //DB에 없는 값 의미
+                };
+
+            //신규 저장에 삽입
+            commit.insert.push(data);
+            //원본 데이터에 삽입
+            commit.list.push(data);
+            //그리드에도 삽입
+            grid.append(data);
+            //강제 입력모드 전환
+            grid.showEditRow(grid.count() - 1, 1);
+            //스크롤해야됨 -_-
+            $('#myGrid').find('tbody').each(function(){this.scrollTop = this.scrollHeight;});
+        });
+        //삭제 버튼
+        $('#btn_delete').click(function(){
+            //선택된 위치 수집
+            var selectedRows = grid.listChecked();
+
+            if(!selectedRows.length){
+                msgbox('Please select row for delete.', 'Remove', {close:true});
+                return;
+            }
+
+            //선택된 위치의 값 수집
+            selectedRows.forEach(function(row){
+                var item = row.data;
+                if(item){
+                    if(!item.isNew){
+                        //DB에 있는 항목만 삭제 대기열 추가
+                        commit.remove.push(item);
+                    }
+
+                    //삽입 및 수정 대기열 있으면 삭제
+                    var idx;
+                    if(~(idx=commit.insert.indexOf(item))){
+                        commit.insert.splice(idx, 1);
+                    }
+                    if(~(idx=commit.update.indexOf(item))){
+                        commit.update.splice(idx, 1);
+                    }
+                }
+                //그리드에서 삭제
+                grid.remove(row.rownum);
+                //원본에서도 삭제
+                commit.list.splice(commit.list.indexOf(item),1);
+
+            });
+
+        });
+        //저장 버튼
+        $('#btn_save').click(function(){
+
+            //편집 모드 비활성화
+            var editrow = grid.getEditRow();
+            if(editrow){
+                grid.hideEditRow(editrow.rownum);
+            }
+
+            //UPDATE 는 쓰레기 값이 들어가 있기 때문에 필터링 후 개수 세기.
+            if(!(commit.insert.length + commit.update.filter(function(a){return a;}).length + commit.remove.length)){
+                msgbox('Nothing changed.', 'Save', {close:true});
+                return;
+            }else if(!confirm('Are you sure to save?')){
+                return;
+            }
+
+            db.begin();
+            //삽입
+            commit.insert.forEach(function(item){
+                if(!item){return;}
+                db.query(
+                    "INSERT INTO insa VALUES (?,?,?,?,?,?,?,?)",
+                    item.id,
+                    item.name,
+                    item.gender,
+                    item.city,
+                    item.age,
+                    item.part,
+                    item.rank,
+                    item.join
+                );
+            });
+            //수정
+            commit.update.forEach(function(item){
+                if(!item){return;}
+                var q = [
+                    'UPDATE insa SET',
+                    '	 name = ? ',
+                    '	,gender = ? ',
+                    '	,city = ? ',
+                    '	,age = ? ',
+                    '	,part = ? ',
+                    '	,rank = ? ',
+                    '	,"join" = ? ',
+                    ' WHERE id = ?'
+                ].join('\n');
+                var values=[];
+                db.query(
+                    q,
+                    item.name,
+                    item.gender,
+                    item.city,
+                    item.age,
+                    item.part,
+                    item.rank,
+                    item.join,
+                    item.id
+                );
+            });
+            //삭제
+            commit.remove.forEach(function(item){
+                if(!item){return;}
+                if(item && !isNaN(item.id)){
+                    db.query("DELETE FROM insa WHERE id = ?", item.id);
+                }
+            });
+            //트랜잭션 실행
+            db.execute(function(){
+                //어자피 새로고침 할거니 취소버튼 기능 실행
+                $('#btn_cancel').click();
+            });
+            beforeProcess(true);
+            afterProcess.isSave = true;
+        });
+        //취소 버튼
+        $('#btn_cancel').click(function(){
+            // 변경내역 삭제
+            for(var tn in commit){
+                commit[tn].length = 0;
+            }
+            //다시 조회 실시
+            db.query("SELECT * FROM insa").then(function(rs){
+                commit.list = [];
+                for(var i=0,len=rs.rows.length;i<len;i++){
+                    var row = rs.rows.item(i), item = {};
+                    //row 객체 항목들이 읽기 전용이므로 쓰기 가능하도록 객체 복사
+                    //이거 안하면 수정이 안됨.
+                    for(var col in row){
+                        Object.defineProperty(item,col,{
+                            __proto__: null,
+                            value: row[col],
+                            writable: true,
+                            enumerable: true,
+                            configurable: false
+                        });
+                    }
+                    commit.list.push(item);
+                }
+                grid.update(commit.list);
+                afterProcess();
+            });
+            beforeProcess();
+        });
+        //불러오거나 저장하기 전 프로세스
+        function beforeProcess(save){
+            if(beforeProcess.called){
+                return;
+            }else{
+                beforeProcess.called = true;
+            }
+            $('button').prop('disabled',true);
+            if(save){
+                msgbox('Saving...', 'Save', {close:false});
+            }
+        }
+        //불러오거나 저장한 후 프로세스
+        function afterProcess(){
+
+            $('button').prop('disabled',false);
+            beforeProcess.called = false;
+
+            if(afterProcess.isSave){
+                msgbox('Saved successfully.', 'Save', {close:true});
+                afterProcess.isSave = false;
+            }else{
+                msgbox.close();
+            }
+        }
+
+    });
+
 	//그리드 열 정의
-	var grid;
+	/*var grid;
 	var columns = [
 	{id: "name", name: "이름", field: "name", width: 100, cssClass:'tc', headerCssClass:'tc', editor: Slick.Editors.Text},
 	{id: "gender", name: "성별", field: "gender", width: 50, cssClass:'tc', headerCssClass:'tc', editor: Slick.Editors.Select(gender), cannotTriggerInsert: true},
@@ -84,303 +355,7 @@
 	{id: "part", name: "부서", field: "part", width: 50, cssClass:'tc', headerCssClass:'tc', editor: Slick.Editors.Select(part), cannotTriggerInsert: true},
 	{id: "rank", name: "직급", field: "rank", width: 50, cssClass:'tc', headerCssClass:'tc', editor: Slick.Editors.Select(rank), cannotTriggerInsert: true},
 	{id: "join", name: "입사일", field: "join", width: 100, cssClass:'tc', headerCssClass:'tc', editor: Slick.Editors.Type('date'), cannotTriggerInsert: true}
-	];
-	//체크박스 활성화 후 맨 처음에 삽입
-	var checkboxSelector = new Slick.CheckboxSelectColumn({
-		cssClass: "slick-cell-checkboxsel"
-	});
-	columns.splice(0,0, checkboxSelector.getColumnDefinition());
+	];*/
 
-	//그리드 옵션
-	var options = {
-		editable: true,
-		enableAddRow: true,
-		enableCellNavigation: true,
-		asyncEditorLoading: true,
-		autoEdit: true,
-		enableColumnReorder: false,
-		forceFitColumns: true,
-		rowHeight: 35
-	};
 
-	// 데이터 관리 확장성을 위한 데이터뷰 초기화
-	var dataView = new Slick.Data.DataView({ inlineFilters: true });
-	//그리드 초기화 후 행 단위 선택으로 정의
-	grid = new Slick.Grid("#myGrid", dataView, columns, options);
-	grid.setSelectionModel(new Slick.RowSelectionModel({selectActiveRow: false}));
-	// 체크박스 모델 초기화
-	grid.registerPlugin(checkboxSelector);
-
-	// 행 변경 발생 시 그리드 새로고침
-	dataView.onRowCountChanged.subscribe(function (e, args) {
-		grid.updateRowCount();
-		grid.render();
-	});
-	// 행 변경 발생시 그리드 새로고침
-	dataView.onRowsChanged.subscribe(function (e, args) {
-		grid.invalidateRows(args.rows);
-		grid.render();
-	});
-
-	// 행 삽입시 기본값 삽입
-	grid.onAddNewRow.subscribe(function (e, args) {
-		var now = new Date();
-		var newid = (function(data){
-			return data.length ? data.slice().sort(function(a,b){
-				return b.id - a.id;
-			})[0].id || 0 : 0;
-		})(dataView.getItems());
-		var item = {
-			"id": newid + 1,
-			"name": "새 이름",
-			"city": "서울",
-			"gender": "남",
-			"age": 20,
-			"part": "총무팀",
-			"rank": "사원",
-			"join": [now.getFullYear(), ('00'+(now.getMonth()+1)).slice(-2), ('00'+(now.getDate())).slice(-2)].join('-'),
-			"isNew": true //DB에 없는 값 의미
-		};
-		$.extend(item, args.item);
-		dataView.addItem(item);
-		//삽입 대기열 추가
-		commit.insert.push(item);
-	});
-
-	// 행 수정시 그리드에 적용
-	grid.onCellChange.subscribe(function (e, args) {
-		dataView.updateItem(args.item.id, args.item);
-		//수정 대기열에 추가. 중복 방지를 위해 기존 행 위치에 삽입하도록
-		commit.update[args.row] = args.item;
-	});
-
-	
-
-	// 행 선택기 작동
-	var columnpicker = new Slick.Controls.ColumnPicker(columns, grid, options);
-
-	// 윈도우 사이즈 조절시 마다 그리드 크기 조정
-	$(window).resize(function(){
-		grid.resizeCanvas();
-	});
-
-	// 검색 시 필터링 작동
-	var form = $('#search_form').submit(function(e){
-		e.preventDefault();
-		var search = this.elements.search, val = search.value;
-
-		Slick.GlobalEditorLock.cancelCurrentEdit();
-		dataView.setFilterArgs({
-			name: val
-		});
-		dataView.refresh();
-
-		return false;
-	});
-
-	// 데이터 필터링 초기화
-	dataView.beginUpdate();
-	dataView.setItems([]);
-	dataView.setFilterArgs({
-		name: ''
-	});
-	dataView.setFilter(function(item, args){
-		if (args.name != "" && item.name.indexOf(args.name) == -1) {
-			return false;
-		}
-
-		return true;
-	});
-	dataView.endUpdate();
-	
-	// 데이터뷰와 그리드 동기화 실시
-	dataView.syncGridSelection(grid, true);
-
-	//신규 버튼
-	$('#btn_insert').click(function(){
-		//편집중에는 제외
-		if (!Slick.GlobalEditorLock.commitCurrentEdit()) {
-			return;
-		}
-
-		//새 입력 항목을 포커스하도록 이벤트 발생
-		grid.setActiveCell(dataView.getLength(), 1);
-		$('.slick-row.active').children('.slick-cell.active').dblclick();
-	});
-	//삭제 버튼
-	$('#btn_delete').click(function(){
-		//선택된 위치 수집
-		var selectedRows = grid.getSelectedRows(), ids = [];
-
-		if(!selectedRows.length){
-			msgbox('삭제할 항목을 선택해 주세요.', '삭제', {close:true});
-			return;
-		}
-
-		//선택된 위치의 값 수집
-		selectedRows.forEach(function(idx){
-			var item = dataView.getItemByIdx(idx);
-			if(item){
-				ids.push(item.id);
-
-				if(!item.isNew){
-					//DB에 있는 항목만 삭제 대기열 추가
-					commit.remove.push(item);
-				}
-
-				//삽입 및 수정 대기열 있으면 삭제
-				var idx;
-				if(~(idx=commit.insert.indexOf(item))){
-					delete commit.insert[idx];
-					commit.insert.length--;
-				}
-				if(~(idx=commit.update.indexOf(item))){
-					delete commit.update[idx];
-					commit.update.length--;
-				}
-				//console.log(commit);
-			}
-		});
-
-		//일괄 삭제 실시
-		dataView.beginUpdate();
-
-		ids.forEach(function(id){
-			dataView.deleteItem(id);
-		});
-
-		dataView.endUpdate();
-
-	});
-	//저장 버튼
-	$('#btn_save').click(function(){
-
-		//UPDATE 는 쓰레기 값이 들어가 있기 때문에 필터링 후 개수 세기.
-		if(!(commit.insert.length + commit.update.filter(function(a){return a;}).length + commit.remove.length)){
-			msgbox('저장할 항목이 없습니다.', '저장', {close:true});
-			return;
-		}
-
-		//선택 상태 적용
-		grid.getEditorLock().commitCurrentEdit();
-
-		db.begin();
-		//삽입
-		commit.insert.forEach(function(item){
-			if(!item){return;}
-			db.query(
-				"INSERT INTO insa VALUES (?,?,?,?,?,?,?,?)",
-				item.id,
-				item.name,
-				item.gender,
-				item.city,
-				item.age,
-				item.part,
-				item.rank,
-				item.join
-				);
-		});
-		//수정
-		commit.update.forEach(function(item){
-			if(!item){return;}
-			var q = [
-			'UPDATE insa SET',
-			'	 name = ? ',
-			'	,gender = ? ',
-			'	,city = ? ',
-			'	,age = ? ',
-			'	,part = ? ',
-			'	,rank = ? ',
-			'	,"join" = ? ',
-			' WHERE id = ?'
-			].join('\n');
-			var values=[];
-			db.query(
-				q,
-				item.name,
-				item.gender,
-				item.city,
-				item.age,
-				item.part,
-				item.rank,
-				item.join,
-				item.id
-				);
-		});
-		//삭제
-		commit.remove.forEach(function(item){
-			if(!item){return;}
-			if(item && !isNaN(item.id)){
-				db.query("DELETE FROM insa WHERE id = ?", item.id);
-			}
-		});
-		//트랜잭션 실행
-		db.execute(function(){
-			//어자피 새로고침 할거니 취소버튼 기능 실행
-			$('#btn_cancel').click();
-		});
-		beforeProcess(true);
-		afterProcess.isSave = true;
-	});
-	//취소 버튼
-	$('#btn_cancel').click(function(){
-		//현재 편집중인 포커스 취소
-		Slick.GlobalEditorLock.cancelCurrentEdit();
-		// 변경내역 삭제
-		for(var tn in commit){
-			commit[tn].length = 0;
-		}
-		//다시 조회 실시
-		db.query("SELECT * FROM insa").then(function(rs){
-			// 데이터를 넣고 필터링을 정의 후 반영
-			dataView.beginUpdate();
-			dataView.setItems([]);
-			for(var i=0,len=rs.rows.length;i<len;i++){
-				var row = rs.rows.item(i), item = {};
-				//row 객체 항목들이 읽기 전용이므로 쓰기 가능하도록 객체 복사
-				//이거 안하면 수정이 안됨.
-				for(var col in row){
-					Object.defineProperty(item,col,{
-						__proto__: null,
-						value: row[col],
-						writable: true,
-						enumerable: true,
-						configurable: false
-					});
-				}
-				dataView.addItem(item);
-			}
-			dataView.setFilterArgs({
-				name: form[0].elements.search.value
-			});
-			dataView.endUpdate();
-			afterProcess();
-		});
-		beforeProcess();
-	});
-	//불러오거나 저장하기 전 프로세스
-	function beforeProcess(save){
-		if(beforeProcess.called){
-			return;
-		}else{
-			beforeProcess.called = true;
-		}
-		$('button').prop('disabled',true);
-		if(save){
-			msgbox('저장 중...', '저장', {close:false});
-		}
-	}
-	//불러오거나 저장한 후 프로세스
-	function afterProcess(){
-
-		$('button').prop('disabled',false);
-		beforeProcess.called = false;
-
-		if(afterProcess.isSave){
-			msgbox('저장되었습니다.', '저장', {close:true});
-			afterProcess.isSave = false;
-		}else{
-			msgbox.close();
-		}
-	}
 });
--- readme.md
+++ readme.md
@@ -1,69 +1,59 @@
-# NodePlatform: node-webkit Business content platform demo
+# NodePlatform: nw.js Business content platform demo
 
 ## Features
 
- - Based on node-webkit (web-based, run anywhere)
+ - Based on nw.js (web-based, run anywhere)
  - synchronied style CRUD (offline comfortability)
  - persistent data store with Web SQL Database
- - You can use any node.js module.
+ - You can use any io.js module.
 
 ## How to run this app
 
- - for node-webkit download when do you not have, go [node-webkit](https://github.com/rogerwang/node-webkit) and see downloads section.
+ - for nw.js download when do you not have, go [nw.js](https://github.com/nwjs/nw.js) and see downloads section.
  - `git clone` or Download zip this project.
  - uncompress at any folder do you like
- - run `nw.exe PROJECT_PATH` or drag project folder to nw.exe on windows.
+ - run `nw(.exe) PROJECT_PATH` or drag project folder to nw.exe on windows.
 
 ## Notes
 
- - this is __quick and dirty__ solution. If do you like this project and apply to real world, you'll need refactory this project.
- - visit [node-webkit wiki](https://github.com/rogerwang/node-webkit/wiki) for more infomation about what framework is, deploy, etc.
+ - this is __quick and dirty__ solution yet. If do you like this project and apply to real world, you'll need refactory this project.
+ - visit [nw.js wiki](https://github.com/nwjs/nw.js/wiki) for more infomation about what framework is, deploy, etc.
 
 ## Used projects in this app
 
- - [node-webkit](https://github.com/rogerwang/node-webkit) (absolutely.)
- - [jQuery](http://jquery.com/) 2.0.3
- - [Bootstrap](http://getbootstrap.com/) 3.0.3
- - [Cosmo](http://bootswatch.com/cosmo/) theme by [Bootswatch](http://bootswatch.com/)
- - [SlickGrid](https://github.com/mleibman/SlickGrid)
+ - [nw.js](https://github.com/nwjs/nw.js) (absolutely!)
+ - [jQuery](http://jquery.com/) 2.1.3
+ - [JUI](https://github.com/seogi1004/jui) Well, He's just an acquaintance. but he was made a great UI framework.
 
 ## TODO
 
- - International support for foreigner. (thanks to Wang for starred this project.)
+ - Web-component???
  - Excel download support
 
-# NodePlatform: node-webkit 로 만든 비즈니스 플랫폼 데모
+# NodePlatform: nw.js 로 만든 비즈니스 플랫폼 데모
 
 ## 특징
 
- - node-webkit 기반으로 웹 개발은 그대로, 응용처럼 어느 플랫폼이던 실행.
+ - nw.js 기반으로 웹 개발은 그대로, 응용처럼 어느 플랫폼이던 실행.
  - 동기화 방식의 CRUD 구현 (오프라인을 위한, 응용 프로그램처럼 설계)
  - Web SQL Database 로 영구적인 저장 기능 구현.
- - node.js 모듈 사용할 수 있다.
+ - io.js 모듈을 사용할 수 있다.
 
 ## 실행 방법
 
- - [node-webkit](https://github.com/rogerwang/node-webkit) 클릭 후 downloads 부분에서 자신에 맞는 OS 프로그램을 다운받는다.
+ - [nw.js](https://github.com/nwjs/nw.js) 클릭 후 downloads 부분에서 자신에 맞는 OS 프로그램을 다운받는다.
  - `git clone` 또는 Download ZIP 버튼을 눌러 저장한다.
- - 원하는 폴더에 압축 푼다.
- - `nw.exe PROJECT_PATH` 실행하거나, 윈도우의 경우 압푹 푼 폴더를 nw.exe 로 드래그한다.
+ - 원하는 폴더에 압축을 푼다.
+ - `nw(.exe) PROJECT_PATH` 실행하거나, 윈도우의 경우 압푹 푼 폴더를 nw.exe 로 드래그한다.
 
 ## 유의사항
 
  - 이 프로젝트는 __quick and dirty__ 솔루션이다. 이 기반으로 운영 전 먼저 이 프로젝트를 다듬을 필요가 있다.
- - 배포 및 기타 참고사항은 [node-webkit wiki](https://github.com/rogerwang/node-webkit/wiki) 문서를 참고한다.
+ - 배포 및 기타 참고사항은 [nw.js wiki](https://github.com/nwjs/nw.js/wiki) 문서를 참고한다.
  - 이 솔루션은 크롬 내장 Web SQL Database 로 데이터를 보관한다. 원격 DB가 있으면 PHP, .NET, JSP 등으로 별도 구현해서 연결해야 한다.
    (물론 node.js 모듈로 다이렉트로 연결 가능하지만, 소스가 공개되는 자바스크립트 특성상 추천하지 않는다.)
- - 원격 접속을 지원하고자 한다면, [package.json](https://github.com/rogerwang/node-webkit/wiki/Manifest-format) 문서를 참고한다.
+ - 원격 접속을 지원하고자 한다면, [package.json](https://github.com/nwjs/nw.js/wiki/Manifest-format) 문서를 참고한다.
 
 ### 도저히 이 데모를 실행할줄 모르겠어. 이게 뭔지도 몰라. 도와줘!
 
 [그림으로 설명해준다. 날 클릭하라.](https://github.com/composite/NodePlatform/wiki)
-
-### 윈도우 사용자를 위해 내가 배려해줬다. 단일 실행 파일 배포한다.
-
-[단일 실행파일 다운로드](http://www.solidfiles.com/d/c5df88cb09/)
-
-그냥 다운받고 실행만 하면 화면 띄워질거다.
-
-오늘로부터 90일 이후 삭제된다. 파일 삭제된거 확인되면 내린다.
